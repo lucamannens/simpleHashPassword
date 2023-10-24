ad0e17ed8658a947e7bff890e4e13da9ce52fcb6
Luca <124049752+zaCodeza@users.noreply.github.com>
2023-10-25 00:22:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Needed for using any Cryptography class like SHA256
+using System.Security.Cryptography;
+// Needed for using Encoding
+using System.Text;
+
+namespace hashPassword {
+    public class Program {
+        static void Main(string[] args) {
+
+            int amount = 0;
+
+            for (int i = 0; i < 3; i++) {
+
+                int a = i;
+                string password = Convert.ToString(a);
+                string hashedPassword = hashPassword(password);
+                Console.WriteLine(hashedPassword);
+                amount++;
+            }
+            Console.WriteLine($"\ntotal amount lines = {amount}\n");
+
+            string example = "jabadabadoe";
+            string hashed = hashPassword(example);
+            Console.WriteLine(hashed);
+
+
+        }
+
+        public static string hashPassword(string password) {
+
+            var sha = SHA256.Create();
+            var asByteArray = Encoding.Default.GetBytes(password);
+            byte[] hashedPassword = sha.ComputeHash(asByteArray);
+            return Convert.ToBase64String(hashedPassword);
+        }
+    }
+}
